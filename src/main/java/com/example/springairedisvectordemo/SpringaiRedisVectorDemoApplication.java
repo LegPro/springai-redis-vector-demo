d21cd1bb758847ced6c5338be20e0d42a9b6a706
Vinay Dadaso Hinukale <jtr529@york.ac.uk>
2024-10-02 11:02:07 +0100

    first commit

---
+++ src/main/java/com/example/springairedisvectordemo/SpringaiRedisVectorDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springairedisvectordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiRedisVectorDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiRedisVectorDemoApplication.class, args);
+	}
+
+}
